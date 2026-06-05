9e908bfebe19160c0e539809ed5ccfb8404f9aaf
alex-isidro <alexander.d.isidro@gmail.com>
2026-06-05 02:57:22 -0300

    feat: adiciona controllers para Fazenda, Perfil, Tanque e Usuario

---
+++ Phycocarbon.API/Controllers/TanqueController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phycocarbon.Application.Services.Interfaces;
+
+namespace Phycocarbon.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class TanqueController(
+    ITanqueService service)
+    : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(service.GetAll());
+    }
+
+    [HttpGet("{id:long}")]
+    public IActionResult GetById(long id)
+    {
+        return Ok(service.GetById(id));
+    }
+}
